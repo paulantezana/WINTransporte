17cccdc26a1fb276fdab4bfc36df4253cd130a70
Roger <bishotb13@gmail.com>
2017-12-10 15:44:08 -0500

    updates

--- Vista/Crud/UCRutas.Designer.cs
+++ Vista/Crud/UCRutas.Designer.cs
@@ -35,7 +35,7 @@
             System.Windows.Forms.Label distanciaLabel;
             System.Windows.Forms.Label cruce_rutaLabel;
             System.Windows.Forms.Label cerradaLabel;
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCRutas));
             this.panel5 = new System.Windows.Forms.Panel();
             this.cerradaCheckBox = new System.Windows.Forms.CheckBox();
@@ -240,7 +240,7 @@
             this.partidaTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.rutasBindingSource, "partida", true));
             this.partidaTextBox.Location = new System.Drawing.Point(27, 90);
             this.partidaTextBox.Name = "partidaTextBox";
-            this.partidaTextBox.Size = new System.Drawing.Size(100, 20);
+            this.partidaTextBox.Size = new System.Drawing.Size(247, 20);
             this.partidaTextBox.TabIndex = 28;
             // 
             // btnNuevo
@@ -352,14 +352,14 @@
             this.rutasDataGridView.AutoGenerateColumns = false;
             this.rutasDataGridView.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(239)))), ((int)(((byte)(242)))));
             this.rutasDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.rutasDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.rutasDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.rutasDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.rutasDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn1,
